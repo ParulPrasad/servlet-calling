27d6d22bb7eb27127d8d99e67d5353be01bc511a
ParulPrasad <59188895+ParulPrasad@users.noreply.github.com>
2020-01-08 11:33:37 +0530

    cube by session

---
+++ cubes.java
@@ -0,0 +1,29 @@
+package com.deloitte;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Cube
+ */
+@WebServlet("/cube")
+public class Cube extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession ssn=request.getSession();
+//		int sum=(int)request.getAttribute("Sum");
+		int sum=(int)(ssn.getAttribute("sum"));
+		int cube=sum*sum*sum;
+		
+	response.getWriter().println("sum: "+sum+"\nCube: "+cube);
+	
+	}
+
+}
