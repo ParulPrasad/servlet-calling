8b7b2e05c2dd4bcbfa90fb5e9004017e2049fc68
ParulPrasad <59188895+ParulPrasad@users.noreply.github.com>
2020-01-08 10:48:22 +0530

    sendRedirect

---
+++ cube.java
@@ -0,0 +1,26 @@
+package com.deloitte;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Cube
+ */
+@WebServlet("/cube")
+public class Cube extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+
+		int sum=Integer.parseInt(request.getParameter("sum"));
+		int cube=sum*sum*sum;
+		response.getWriter().println("sum: "+sum+"\nCube: "+cube);
+	}
+
+}
